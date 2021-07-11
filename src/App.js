bccb7e999049f7a96be564fb90e3c90070e03d09
vasudev <palojuvasudev05@gmail.com>
2021-07-11 23:31:44 +0530

    July 10,11 Introduction to React.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react';
+
+//When a parent component state changes all the children under that will be updated or re-rendered
+export class ParentComponent extends React.Component {
+  constructor(){
+    console.log('I am constructor')
+    super();
+    //State Intitation which will happen only once.
+    this.state = {
+      names: ['vasudev', 'hunter', 'suneel', 'john', 'Santos' , 'Darek', 'Ridwane'],
+    }
+  }
+
+  
+
+  componentDidMount(){
+    //We can make an API call to the backend server.
+    console.log('I am componentDid Mount')
+  }
+
+  componentDidUpdate(){
+    //We can make an API call here 
+    console.log('I am componentDidUpdate')
+  }
+
+  render(){
+    console.log('I am render')
+    return (
+      <div>
+        <h1>I am Parent </h1>
+        <button></button>
+      </div>
+    )
+  }
+}
+class ChildComponent extends React.Component{
+  constructor(){
+    super();
+    console.log('I am constructor:CHILD');
+  }
+  componentDidMount(){
+
+    //child u made an api call -> you got updated data 
+    console.log('I am componentDidMount:CHILD')
+  }
+  componentDidUpdate(){
+    console.log('I am componentDidUpdate:CHILD')
+  }
+  render(){
+    console.log('I am render:CHILD')
+    return (
+      <>
+        <h1 onClick={()=>alert(this.props.name)}>{this.props.index}.) My name is {this.props.name}</h1>
+      </>
+    )
+  }
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { ParentComponent } from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ParentComponent />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('eduonix')
 );
 
 // If you want to start measuring performance in your app, pass a function
